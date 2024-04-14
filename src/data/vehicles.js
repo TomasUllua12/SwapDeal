d34257a43d5458b389f84d3dd1d7170d2d0483ea
TomasUllua12 <tomasullua@gmail.com>
2024-04-14 14:01:01 -0300

    Agregué algunos artículos

--- src/data/vehicles.js
+++ src/data/vehicles.js
@@ -1,44 +1,32 @@
 const vehicles = [
-    {
-      name: "Car",
-      description: "has four wheels",
-      image:
-        "https://thumbor.forbes.com/thumbor/fit-in/1200x0/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5d35eacaf1176b0008974b54%2F0x0.jpg%3FcropX1%3D790%26cropX2%3D5350%26cropY1%3D784%26cropY2%3D3349",
-    },
-    {
-      name: "Motorcycle",
-      description: "goes fast",
-      image:
-        "https://content2.kawasaki.com/ContentStorage/KMC/Products/8798/8c85b065-f4e9-4ee6-966f-2b7993daff08.png?w=767",
-    },
-    {
-      name: "Bycicle",
-      description: "it's good for the environment",
-      image:
-        "https://media.wired.com/photos/63e569c9de59d567d5d7c66d/master/w_2240,c_limit/Ride1Up-Cafe-Cruiser-Featured-Gear.jpg",
-    },
-    {
-      name: "Plane",
-      description: "Tickets are expensive",
-      image: "https://www.rd.com/wp-content/uploads/2020/01/GettyImages-1131335393-e1650030686687.jpg",
-    },
-    {
-      name: "Ship",
-      description: "It is heavy",
-      image: "https://www.marineinsight.com/wp-content/uploads/2019/08/Cruise-ships-1.png",
-    },
-    {
-      name: "Spaceship",
-      description: "Goes to space",
-      image:
-        "https://img.freepik.com/premium-photo/futuristic-scifi-battle-spaceship-hover-ocean-alien-planet-digital-painting_379823-2914.jpg?w=2000",
-    },
-    {
-        name: "Spaceship",
-        description: "Goes to space",
-        image:
-          "https://img.freepik.com/premium-photo/futuristic-scifi-battle-spaceship-hover-ocean-alien-planet-digital-painting_379823-2914.jpg?w=2000",
-      },
-  ];
-  
-  export default vehicles;
+  {
+    name: "Monitor GADNIC 16p Bla Bla",
+    description: "Monitor en perfecto estado con poco uso...",
+    categoria: "Tecnologia",
+    image:
+      "https://cdn.pixabay.com/photo/2017/07/11/11/54/imac-2493287_1280.png",
+  },
+  {
+    name: "Notebook HP i7 Bla Bla",
+    description: "Notebook en perfecto estado con poco uso...",
+    categoria: "Tecnologia",
+    image:
+      "https://cdn.pixabay.com/photo/2019/04/22/13/21/computer-4146579_1280.jpg",
+  },
+  {
+    name: "El Campito - Da Vinci 1753 Bla Bla",
+    description: "Cuadro hecho por Da Vinci en su paso por Córdoba",
+    categoria: "Hogar",
+    image:
+      "https://cdn.pixabay.com/photo/2017/10/29/23/11/painting-2901057_1280.jpg",
+  },
+  {
+    name: "Olla Essen",
+    description: "Olla en perfecto estado con poco uso...",
+    categoria: "Cocina",
+    image:
+      "https://cdn.pixabay.com/photo/2014/12/02/14/50/pot-554068_1280.jpg",
+  },
+];
+
+export default vehicles;
