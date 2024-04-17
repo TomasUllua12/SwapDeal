a39e412a60e34f79fcb9fc81181057346c2cac88
TomasUllua12 <tomasullua@gmail.com>
2024-04-17 17:21:43 -0300

    Footer 1.01

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,29 +11,30 @@ export function Footer(props) {
                     <div class="box">
                         <figure>
                             <a href="#">
-                                
+                                <img scr="../../public/assets/icons/LogoynombreSwapDealBlanco.svg" alt="Logo SwapDeal"/>
                             </a>
                         </figure>
                     </div>
                     <div class="box">
                         <h2>SOBRE NOSOTROS</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ipsa?</p>
+                        <p>¡Intercambia lo que ya no usas!</p>
+                        <p>En SwapDeal encuentras lo que buscas sin gastar dinero.</p>
+                        <p>Gratis, fácil y seguro. ¡Empieza a permutar hoy mismo!</p>
                     </div>
                     <div class="box">
                         <h2>ENCONTRANOS EN</h2>
                         <div class="red-social">
-                            <div><a href="#" class="fa fa-facebook"></a></div>
-                            <div><a href="#" class="fa fa-twitter"></a></div>
-                            <div><a href="#" class="fa fa-intagram"></a></div>
-                            <div><a href="#" class="fa fa-youtube"></a></div>
+                            <div><a href="#" class="fab fa-facebook"></a></div>
+                            <div><a href="#" class="fab fa-twitter"></a></div>
+                            <div><a href="#" class="fab fa-intagram"></a></div>
+                            <div><a href="#" class="fab fa-youtube"></a></div>
                         </div>
                     </div>
                 </div>
                 <div class="grupo-2">
                     <small>&copy; 2024 <b>SwapDeal</b> - Todos los Derechos Reservados.</small>
                 </div>
-        </footer>
+            </footer>
         </>
     )
 }
