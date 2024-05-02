61aa1e5476bdedfba926408db51e120f56d839f9
TomasUllua12 <tomasullua@gmail.com>
2024-05-02 13:12:02 -0300

    Login/Register Hecho

--- src/views/LoginRegister.jsx
+++ src/views/LoginRegister.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
+import "./LoginRegister.css"
 
 export function LoginRegister(props) {
     
  
     return (
         <>
-            <h1>Login Register</h1>
+            <body>
+                <div className='wrapper'></div>
+                <div className='overlay'>
+                    <div className='container'>
+                        <div className='main'>
+                            <input type="checkbox" id='chk' aria-hidden="true" />
+
+                            <div className='login'>
+                                <form className='form'>
+                                    <label htmlFor="chk" aria-hidden="true">Iniciar sesión</label>
+                                    <input className="input" type="email" autoComplete='off'
+                                    name='email' placeholder='Email' required=""/>
+                                    <input className="input" type="password"
+                                    name='pswd' placeholder='Password' required=""/>
+                                    <button>Iniciar sesión</button>
+                                </form>
+                            </div>
+                            
+                            <div className='register'>
+                                <form className='form'>
+                                    <label htmlFor="chk" aria-hidden="true">Creá tu cuenta</label>
+                                    <input className="input" type="text" autoComplete='off'
+                                    name='txt' placeholder='Username' required=""/>
+                                    <input className="input" type="email" autoComplete='off'
+                                    name='email' placeholder='Email' required=""/>
+                                    <input className="input" type="password"
+                                    name='pswd' placeholder='Password' required=""/>
+                                    <button>Crear</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </body>
+            
         </>
     )
 }
