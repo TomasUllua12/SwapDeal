bfe3f17c287bb3233ade0aeb8c9a90921458b0c6
TomasUllua12 <tomasullua@gmail.com>
2024-05-03 08:44:49 -0300

    Correcciones

--- src/views/pagina_principal/PaginaPrincipal.jsx
+++ src/views/pagina_principal/PaginaPrincipal.jsx
@@ -70,7 +70,6 @@ export function PaginaPrincipal(props) {
                     <h2>Conócelo Ahora.</h2>
                 </section>
                 <section className="main-exchange-container">
-                    <div className="backgroundImg"></div>
                     <div className="main-exchange-container--title">
                         <h2>Visibilizamos todas las tasas de cambio.</h2>
                         <p>Traemos información en tiempo real de las casas
