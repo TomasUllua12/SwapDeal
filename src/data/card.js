541cb879eb46781c06f4ac6ee17df9670750a1c0
TomasUllua12 <tomasullua@gmail.com>
2024-05-15 14:48:56 -0300

    Cambie la pagina de categorias

---
+++ src/data/card.js
@@ -0,0 +1,16 @@
+const cards = [
+    {
+      name: "Cocina",
+      color: "#470000"
+    },
+    {
+      name: "Tecnología",
+      color: "#870000"
+    },
+    {
+      name: "Hogar",
+      color: "#994000"
+    },
+  ];
+  
+  export default cards;
