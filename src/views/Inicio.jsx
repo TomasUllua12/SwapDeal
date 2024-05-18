d29a87e26be61fff0b3418a38bf92d4839b62104
simon2432 <simonfeeney11@gmail.com>
2024-05-18 11:19:28 -0300

    cambios inicio

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -14,13 +14,28 @@ export function Inicio(props) {
   return (
     <>
       <Header />
+
       <main className="inicio-main">
-        <div className="inicio-main-container">
-          <div className="container">{vehicleList}</div>
-          <ShowHide />
-          <div className="espacio"></div>
+        <div className="fondo-video">
+          
+        </div>
+
+        <div className="grid-informacion">
+          <div className="secciones-grid">
+            <div className="mundo-cambiante">mundo girando</div>
+            <div className="cantidad-permutas">permutas en la ultima hora 10</div>
+            <div className="comentarios-positivos">esta aplicacion me dejo obtener mis cosas preferidas</div>
+            <div className="seguirdad-swapdeal">swap deal cuenta con verificacion constante</div>
+            <div className="categorias-mas-intercambiada">la categoria mas permutada es hogar</div>
+            <div className="informacion-sobre-app">swap deal mira las mejores practicas dentro de la aplicacion</div>
+          </div>
+        </div>
+
+        <div className="seccion-articulos-recomendados">
+
         </div>
       </main>
+      
       <FooterWave />
     </>
   );
