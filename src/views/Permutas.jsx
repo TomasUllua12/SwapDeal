adf7bdee4df2b6ef37297e6c7d2340378e17659f
TomasUllua12 <tomasullua@gmail.com>
2024-05-19 16:15:04 -0300

    Cree las secciones de la pestaña permutas

--- src/views/Permutas.jsx
+++ src/views/Permutas.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import "./Permutas.css"
+import React from "react";
+import "./Permutas.css";
 import { Header } from "../components/Header";
 import FooterWave from "../components/Footers/FooterWave";
 
 export function Permutas(props) {
-    
-
-    return (
-        <>
-            <Header/>
-            <main className='main-permutas'>
-                
-            </main>
-            <FooterWave/>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <main className="main-permutas">
+        <h2 className="main-permutas-solicitudes-title">
+          Solicitudes de Permuta
+        </h2>
+        <section className="main-permutas-solicitudes">
+          <div className="main-permutas-solicitudes__container"></div>
+        </section>
+        <h2 className="main-permutas-permutas-title">Permutas</h2>
+        <section className="main-permutas-permutas">
+          <div className="main-permutas-permutas__container"></div>
+        </section>
+        <h2 className="main-permutas-historial-title">Historial de Permutas</h2>
+        <section className="main-permutas-historial">
+          <div className="main-permutas-historial__container"></div>
+        </section>
+      </main>
+      <FooterWave />
+    </>
+  );
 }
