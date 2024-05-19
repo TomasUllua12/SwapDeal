ae6cb5f7098365a111c896c4a6d9f6e7026db9a0
TomasUllua12 <tomasullua@gmail.com>
2024-05-19 15:54:57 -0300

    modificando seccion permutas

--- src/views/Permutas.jsx
+++ src/views/Permutas.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import "./Permutas.css"
+import { Header } from "../components/Header";
+import FooterWave from "../components/Footers/FooterWave";
 
 export function Permutas(props) {
     
 
     return (
         <>
-            
+            <Header/>
+            <main className='main-permutas'>
+                
+            </main>
+            <FooterWave/>
         </>
     )
 }
