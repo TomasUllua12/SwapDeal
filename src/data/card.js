ac597cb36e4ea362d349d59dcc6f57b0b3676a7c
simon2432 <simonfeeney11@gmail.com>
2024-05-22 08:38:52 -0300

    cambio-categorias

--- src/data/card.js
+++ src/data/card.js
@@ -1,23 +1,53 @@
 const cards = [
     {
-      name: "Cocina",
-      color: "#470000",
-      img: "../../public/assets/img/categorias/cocina.jpg"
-    },
-    {
-      name: "Tecnología",
-      color: "#870000",
-      img: "../../public/assets/img/categorias/tecnologia.jpg"
-    },
-    {
-      name: "Hogar",
-      color: "#994000",
+      name: "Hogar y muebles",
+      color: "#8B4513",
       img: "../../public/assets/img/categorias/hogar.jpg"
     },
     {
-      name: "Baño",
-      color: "#294000",
-      img: "../../public/assets/img/categorias/bano.jpg"
+      name: "Tecnología",
+      color: "#1E90FF",
+      img: "../../public/assets/img/categorias/tecno.jpg"
+    },
+    {
+      name: "Moda y accesorios",
+      color: "#FF69B4",
+      img: "../../public/assets/img/categorias/moda.jpg"
+    },
+    {
+      name: "Deportes",
+      color: "#32CD32",
+      img: "../../public/assets/img/categorias/deportes.jpeg"
+    },
+    {
+      name: "Entretenimiento",
+      color: "#FFD700",
+      img: "../../public/assets/img/categorias/libro.jpg"
+    },
+    {
+      name: "Vehiculos",
+      color: "#FF4500",
+      img: "../../public/assets/img/categorias/auto.jpg"
+    },
+    {
+      name: "Herramientas y materiales",
+      color: "#4682B4",
+      img: "../../public/assets/img/categorias/herra.jpg"
+    },
+    {
+      name: "Salud y belleza",
+      color: "#FFB6C1",
+      img: "../../public/assets/img/categorias/salud.jpg"
+    },
+    {
+      name: "Mascotas",
+      color: "#FF6347",
+      img: "../../public/assets/img/categorias/mascotas.jpg"
+    },
+    {
+      name: "Variedades",
+      color: "#6A5ACD",
+      img: "../../public/assets/img/categorias/vaire.jpg"
     },
   ];
   
