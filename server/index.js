3e4629e4cb868b4f9d8f4063d4a2ec5a6a64a886
antonioamu <65611035+antonioamu@users.noreply.github.com>
2024-06-10 17:32:10 -0300

    Probando

--- server/index.js
+++ server/index.js
@@ -9,8 +9,8 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "123456",
-    database: "swap_deal"
+    password: "",
+    database: "swapdeal"
 });
 
 db.connect((err) => {
