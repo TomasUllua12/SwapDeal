92a34738fbbae887b4f4f7adc2905c6c33f36c05
simon2432 <simonfeeney11@gmail.com>
2024-06-14 00:58:06 -0300

    buscar-articulos-proceso

--- client/src/components/Articulo.jsx
+++ client/src/components/Articulo.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import "./Articulo.css";
 import { Link } from "react-router-dom";
 
-export function Articulo({ title }) {
-  return (
-    <>
-      <div className="articulo-container">
-        <section className="articulo-categoria-container"><h4>Tecnología</h4></section>
-        <section className="articulo-image-container"></section>
-        <section className="articulo-button-container">
-          <Link className="articulo-button-container-link" to={title}>
-            Ver Artículo
-          </Link>
-        </section>
-      </div>
-    </>
-  );
+function Articulo({ articulo }) {
+    return (
+        <div className="articulo-container">
+            <section className="articulo-categoria-container">
+                <h4>{articulo.categoria}</h4>
+            </section>
+            <section className="articulo-image-container">
+                <img  src={articulo.imagen} alt={articulo.titulo} />
+            </section>
+            <section className="articulo-button-container">
+                <Link className="articulo-button-container-link" to={`/Articulo/${articulo.id}`}>
+                    Ver Artículo
+                </Link>
+            </section>
+        </div>
+    );
 }
+
+export default Articulo;
